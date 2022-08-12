7874cdc3349925faf2f1a4024b3bb9a5b8a27580
twoplusfiveis <111101689+twoplusfiveis@users.noreply.github.com>
2022-08-11 23:26:33 -0400

    Add files via upload

---
+++ Obby_Destoryer.lua
@@ -0,0 +1,5 @@
+-- Obby Destoryer By @Dyll#0002 , @brr#4379
+-- Open Source, TP's and beats all checkpoints in games with the checkpoint systems used in the Source!
+-- Enjoy!
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/vbrr/ObbyScript/main/Obby.lua", true))()
